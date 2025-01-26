b94bdeab4f2fe3305b6f5dda7bdbda9a9b5ceeb2
Mauro <mauro.pango@siemens.com>
2025-01-26 10:58:13 +0000

    commented sns to test go unit test

--- sns.tf
+++ sns.tf
@@ -9,12 +9,12 @@ resource "aws_sns_topic" "billing_alerts" {
 }
 
 # Subscription for the SNS topic to send email notifications to the user.
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  provider = aws.us_east_1
-  topic_arn = aws_sns_topic.billing_alerts.arn
-  protocol  = "email"
-  endpoint  = "your-email"
-}
+# resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+#   provider = aws.us_east_1
+#   topic_arn = aws_sns_topic.billing_alerts.arn
+#   protocol  = "email"
+#   endpoint  = "your-email"
+# }
 
 
 
